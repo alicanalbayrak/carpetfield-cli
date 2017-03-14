a44b29beb38529197e7d8e65860582a6aa49673c
Ali Can Albayrak <albayrakalican@gmail.com>
2017-03-14 17:45:22 +0300

    match stub

---
+++ src/app/game/Match.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by alicana on 14/03/2017.
+ */
+export class Match {
+  constructor(public organization: string,
+              public match: string,
+              public place: string,
+              public date: string) {
+  }
+}
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Match} from './Match';
 
 @Component({
   selector: 'app-game',
@@ -6,9 +7,35 @@ import {Component, OnInit} from '@angular/core';
     <div class="page-header">
         <h1>Match</h1>      
     </div>
+    
+  <table class="table table-striped">
+  <tr>
+    <th>Organizasyon</th>
+    <th>Mac</th>
+    <th>Yer</th>
+    <th>Zaman</th>
+    <th></th>
+    <th></th>
+  </tr>
+  <tr *ngFor="let game of games">
+    <td>{{game.organization}}</td>
+    <td>{{game.match}}</td>
+    <td>{{game.place}}</td>
+    <td>{{game.date}}</td>
+    <td><button type="button" class="btn btn-success">Success</button></td>
+    <td><button type="button" class="btn btn-danger">Danger</button></td>
+  </tr>
+</table>
 `
 })
+
 export class GameComponent implements OnInit {
+
+  games = [
+      new Match("AYESAS", "Haftalık maçımız", "kılıçarslan", "10 aralık"),
+      new Match("GOOGLE", "Aylık maçımız", "Nevada", "12 aralık")
+  ];
+
 
   constructor() {
   }
