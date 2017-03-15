2517b5d35d87dd5e24ef941cd216a62f7bf4394d
Ali Can Albayrak <albayrakalican@gmail.com>
2017-03-15 21:16:04 +0300

    Http post example

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,37 +1,50 @@
 import {Injectable} from '@angular/core';
+import {Headers, RequestOptions, Http} from "@angular/http";
 
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/delay';
+import {Observable} from 'rxjs/Rx';
+// Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
-export interface IUser {
+export interface ICurrentUser {
   id: string,
-  name: string,
   email: string,
-  password: string
+  valid: boolean
 }
 
 @Injectable()
 export class AuthService {
 
-  currentUser: IUser;
+  currentUser: ICurrentUser;
 
   // store the URL so we can redirect after logging in
   redirectUrl: string;
 
-  login(email: string, password: string): Observable<boolean> {
-    return Observable.of(true).delay(1000).do(
-      val => this.currentUser = {
-        id: "1",
-        name: "alican",
-        email: email,
-        password: password
-      }
-    );
+  constructor(private http: Http) {
+
   }
 
-  isAuthenticated(){
+  login(email: string, password: string): Observable<boolean> {
+
+    let headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+    let options = new RequestOptions({headers: headers});
+
+    return this.http
+      .post("http://localhost:8080/login", JSON.stringify({
+        'email': email,
+        'password': password
+      }), {headers: headers})
+      .map(res => {
+          this.currentUser = res.json().valid ? res.json() as ICurrentUser : null;
+        }
+      )
+      .catch((error: any) => Observable.throw(error.json() || 'server error'));
+
+  }
+
+  isAuthenticated() {
     return !!this.currentUser;
   }
 
